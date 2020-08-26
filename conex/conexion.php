048a91e91914f15a865fa22a9eebe13d1a360cd1
jorgeochoa1987 <jeob2046@gmail.com>
2020-08-26 12:18:31 -0500

    Add ignore

--- conex/conexion.php
+++ conex/conexion.php
@@ -1,14 +1,14 @@
 <?php
 
 
-                    
-   $conexion = new mysqli("localhost", "u876798985_spmensajeria", "6T?m~8nPV:f","u876798985_spmensajeria");
 
-   if($conexion === false) {  
+   $conexion = new mysqli("localhost", "Adminmensajeria", "Adminmensajeria", "mensajeria");
+
+   if($conexion === false) { 
       echo 'Ha habido un error <br>'.mysqli_connect_error(); 
      } else {
   
-  
+   
       
      } 
 
