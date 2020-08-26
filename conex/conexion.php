d37db7f9619a061a15de0020bdcb21e76780e145
jorgeochoa1987 <jeob2046@gmail.com>
2020-08-26 12:14:28 -0500

    Ajuste de conexión

--- conex/conexion.php
+++ conex/conexion.php
@@ -1,8 +1,8 @@
 <?php
 
 
-
-   $conexion = new mysqli("localhost", "u876798985_spmensajeria", "u876798985_spmensajeria", "6T?m~8nPV:f");
+                    
+   $conexion = new mysqli("localhost", "u876798985_spmensajeria", "6T?m~8nPV:f","u876798985_spmensajeria");
 
    if($conexion === false) {  
       echo 'Ha habido un error <br>'.mysqli_connect_error(); 
